89b56c2680bbaa2e2c13da0ff7717a0a40bff7c1
Tiphaine <tiphainedurand44@gmail.com>
2024-11-08 10:28:06 +0100

    💚 Fix CI

--- bump-version.js
+++ bump-version.js
@@ -5,19 +5,27 @@ import { execSync } from "child_process";
 
 (async () => {
   // Afficher un prompt pour choisir le type de bump
-  const response = await prompts({
-    type: "select",
-    name: "bumpType",
-    message: "Choisissez le type d'incrémentation de version :",
-    choices: [
-      { title: "Patch", value: "patch" },
-      { title: "Minor", value: "minor" },
-      { title: "Major", value: "major" },
-    ],
-    initial: 0,
-  });
+  const response = await prompts([
+    {
+      type: "select",
+      name: "bumpType",
+      message: "Choisissez le type d'incrémentation de version :",
+      choices: [
+        { title: "Patch", value: "patch" },
+        { title: "Minor", value: "minor" },
+        { title: "Major", value: "major" },
+      ],
+      initial: 0,
+    },
+    {
+      type: "text",
+      name: "description",
+      message: "Entrez une description pour le commit de version :",
+      initial: "Améliorations et corrections",
+    },
+  ]);
 
-  const bumpType = response.bumpType;
+  const { bumpType, description } = response;
 
   // Si l'utilisateur n'a pas choisi d'option
   if (!bumpType) {
@@ -26,8 +34,11 @@ import { execSync } from "child_process";
   }
 
   try {
-    // Exécuter la commande pnpm pour incrémenter la version
-    execSync(`pnpm version ${bumpType}`, { stdio: "inherit" });
+    // Utiliser npm version avec un message de commit personnalisé
+    execSync(`pnpm version ${bumpType} -m "publish v%s - ${description}"`, {
+      stdio: "inherit",
+    });
+
     console.log(`Version incrémentée en ${bumpType} avec succès !`);
   } catch (error) {
     console.error("Erreur lors de l'incrémentation de la version :", error.message);
